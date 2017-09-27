f7fe9f08e8779957a522c01387b97c1498d333ec
wangl7 <liang.wang@dianrong.com>
2017-09-27 23:21:12 +0800

    add user form

--- hello.py
+++ hello.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'HARD TO GUESS STRING'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
@@ -21,15 +25,24 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data=''
+    return render_template('index.html', form=form, name=name)
 
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+class NameForm(Form):
+    name = StringField('what is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 
 if __name__ == '__main__':
