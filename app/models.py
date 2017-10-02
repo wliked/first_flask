2c944b5e230db1571501d2bc055f6f0f96f1ffc6
wangl7 <liang.wang@dianrong.com>
2017-10-02 22:44:32 +0800

    add user confirm function

--- app/models.py
+++ app/models.py
@@ -14,12 +14,30 @@ class Role(db.Model):
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
+
+    confirmed = db.Column(db.Boolean, default=False)
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
 
+    def generate_confirmation_toke(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True   
+        
     def __repr__(self):
         return '<User %r>' % self.username
 
